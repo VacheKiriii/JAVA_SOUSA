99c50fc3367c4480fd9081a7596a63c27a64c937
Mathys <77534645+VacheKiriii@users.noreply.github.com>
2024-09-04 16:53:20 +0200

    Add files via upload
    Dépôt premier projet classe Voyage

---
+++ EasyLine/src/Main.java
@@ -0,0 +1,13 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        Voyage alger = new Voyage();
+        alger.setDestination("Kasba");
+        alger.setDuree(2);
+        alger.setPrix(3000);
+        System.out.println("La destination est " + alger.getDestination());
+        System.out.println("La duree est de " + alger.getDuree() + " semaines ");
+        System.out.println("Le prix est de " + alger.getPrix() + " euros ");
+    }
+}
---
+++ EasyLine/src/Voyage.java
@@ -0,0 +1,26 @@
+public class Voyage {
+    private String destination ;
+    private int duree ;
+    private double prix ;
+
+    public String getDestination() {
+        return destination;
+    }
+    public void setDestination(String destination) {
+        this.destination = destination;
+    }
+
+    public int getDuree() {
+        return duree;
+    }
+    public void setDuree(int duree) {
+        this.duree = duree;
+    }
+
+    public double getPrix() {
+        return prix;
+    }
+    public void setPrix(double prix) {
+        this.prix = prix;
+    }
+}
